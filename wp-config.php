a0278641299dbf051fe163f1be5423d067fd43d7
Cedric Johnson <cjohns0913@gmail.com>
2018-10-15 16:13:16 -0500

    Test

--- wp-config.php
+++ wp-config.php
@@ -23,7 +23,7 @@
 define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'wordpress');
+define('DB_USER', 'wordpressuser');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'W0RDPRE$$user504');
